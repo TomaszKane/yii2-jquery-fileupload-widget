5f34873e4d9ed9251020493f9dbe2481b937f45b
kane <kane@skytechnology.pl>
2016-12-23 10:49:26 +0100

    Add polish translation

---
+++ src/messages/pl/jqueryfileupload.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'Add files' => 'Dodaj pliki',
+    'Start upload' => 'Wyślij',
+    'Cancel upload' => 'Przerwij wysyłanie',
+    'Start' => 'Start',
+    'Cancel' => 'Anuluj',
+    'Error' => 'Błąd',
+    'Delete' => 'Usuń',
+    'Processing' => 'Przetwarzanie',
+];
